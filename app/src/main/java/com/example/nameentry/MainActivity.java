14490c6e3d3583eb210db41db8526eb997f1cc83
mmendez9 <mmendez9@dtcc.edu>
2017-02-01 12:08:06 -0500

    Message

---
+++ app/src/main/java/com/example/nameentry/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.nameentry;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void btnClick(View view) {
+        // Create a TextView and EditText
+        TextView text = (TextView) findViewById(R.id.Nametf);
+        EditText name = (EditText) findViewById(R.id.nameInput);
+
+        Log.d("Name", "Hello " + name.getText().toString());
+
+        // Snackbar
+        Snackbar.make(view, "Hello " + name.getText().toString() , Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+
+        // Toast
+        Toast.makeText(getApplicationContext(), "Hello " + name.getText().toString(),
+                Toast.LENGTH_LONG).show();
+
+        // Put the name in the textview
+        text.setText("Hello " + name.getText().toString());
+    }
+}
